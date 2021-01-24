004bf42d43273d6c0b659cb1c6279128cd4b6b9b
Hayden Pfeiffer <15hp19@queensu.ca>
2021-01-24 07:25:42 -0500

    Added Audio upload button

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ class App extends Component {
           <h3>Admin Stuff</h3>
           <ScreamUpload></ScreamUpload>
           <MIDIUpload></MIDIUpload>
+          <AudioUpload></AudioUpload>
         </div>
         <div className="App-body">
           <Navbar fixed="bottom" bg="dark" variant="dark" className="justify-content-center">
@@ -316,7 +317,7 @@ class MIDIUpload extends Component {
 
   render() {
     return (<div className="retro">
-      <h5>Add a New Song</h5>
+      <h5>Add a New Song (Midi)</h5>
       <Form onSubmit={this.handleSubmit}>
 
         <Form.Group as={Row} controlId="formHorizontalName">
@@ -325,6 +326,135 @@ class MIDIUpload extends Component {
           </Form.Label>
           <Col sm={10}>
             <Form.Control type="text" placeholder="Song Name" name="name" value={this.state.name} onChange={this.handleChange} />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} controlId="formHorizontalName">
+          <Form.Label column sm={2}>File</Form.Label>
+          <Col sm={10}>
+            <Form.File
+              name="file"
+              onChange={this.handleFileChange}
+              id="custom-file"
+              label={this.state.filename}
+              custom
+            /></Col>
+        </Form.Group>
+        {this.state.submitting == true ? <ProgressBar animated now={this.state.progress} /> :
+          <Button className="retro rbtn-small" type="submit">
+            {this.state.submittext}
+          </Button>
+
+        }
+      </Form>
+
+    </div>
+    );
+  }
+}
+
+class AudioUpload extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    if (this.state.name != '' && this.state.file != '') {
+      this.setState({ ...this.state, submittext: 'Uploading...', submitting: true });
+      var db = firebase.firestore();
+      var storageRef = firebase.storage().ref();
+
+      var doc = db.collection("audio").doc();
+      doc.set({
+        name: this.state.name
+      })
+        .then(() => {
+          console.log("Document written with ID: ", doc.id);
+
+          // Create a reference to file
+          var screamFileRef = storageRef.child('audio/' + doc.id);
+          var uploadTask = screamFileRef.put(this.state.file);
+
+          uploadTask.on('state_changed',
+            (snapshot) => {
+              // Observe state change events such as progress, pause, and resume
+              // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+              var uploadprogress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              this.setState({ ...this.state, progress: uploadprogress });
+              console.log('Upload is ' + uploadprogress + '% done');
+              switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                  console.log('Upload is paused');
+                  break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                  console.log('Upload is running');
+                  break;
+              }
+            },
+            (error) => {
+              // Handle unsuccessful uploads
+            },
+            () => {
+              // Handle successful uploads on complete
+              uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                console.log('File available at', downloadURL);
+
+                doc.update({
+                  url: downloadURL
+                }).then((doc) => {
+                  console.log("Document updated");
+                  // Reset form
+                  this.setState({ ...this.state, name: '', filename: "select an Audio (wav) file...", file: '', submittext: 'Upload', submitting: false, progress: 0 });
+                })
+                  .catch((error) => {
+                    console.error("Error updating document: ", error);
+                    // Reset form
+                    this.setState({ ...this.state, name: '', filename: "select an Audio (wav) file...", file: '', submittext: 'Upload', submitting: false, progress: 0 });
+                  });
+              });
+            }
+          );
+
+        })
+        .catch(function (error) {
+          console.error("Error adding document: ", error);
+        });
+    } else {
+      console.log("MUST FILL INFO");
+    }
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { name: '', filename: "select a file...", file: '', submittext: 'Upload', submitting: false, progress: 0 };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleFileChange(event) {
+    this.setState({
+      ...this.state,
+      filename: event.target.files[0].name,
+      [event.target.name]: event.target.files[0]
+    });
+  }
+
+  render() {
+    return (<div className="retro">
+      <h5>Add a New Song (Audio)</h5>
+      <Form onSubmit={this.handleSubmit}>
+
+        <Form.Group as={Row} controlId="formHorizontalName">
+          <Form.Label column sm={2}>
+            Name
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="text" placeholder="Audio Name" name="name" value={this.state.name} onChange={this.handleChange} />
           </Col>
         </Form.Group>
         <Form.Group as={Row} controlId="formHorizontalName">
